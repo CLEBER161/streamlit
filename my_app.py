555d9fa2babac17e3194e178968c34789576ad16
CLEBER161 <81722017+CLEBER161@users.noreply.github.com>
2024-03-01 11:11:45 -0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -3,7 +3,7 @@ import os
 from numpy.random import randn
 import streamlit as st
 #import matplotlib.pyplot as plt
-import openpyxl
+#import openpyxl
 from streamlit_card import card
 import plotly.graph_objects as go
 import plotly.express as px
